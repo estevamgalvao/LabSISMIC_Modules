88ce80e592bad5fd4135def301e71c8e6af15190
Estevam Albuquerque <estevamgalvao@gmail.com>
2019-04-15 12:48:44 -0300

    solved LED high Z

--- Module1/Module1_Study/main.asm
+++ Module1/Module1_Study/main.asm
@@ -24,32 +24,43 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 ; Main loop here
 ;-------------------------------------------------------------------------------
 
+;-------------------------------------------------------------------------------
+; PRIMEIRA TENTATIVA
+;
 configPins:
-			bic.b	#(BIT5|BIT6), &P5DIR
-			bis.b	#(BIT5|BIT6), &P5REN 	;botão é bit clear nas resistências?
-			bis.b	#(BIT5|BIT6), &P5OUT 	;pullup
+			bic.b	#LOCKLPM5, &PM5CTL0		; Disable the GPIO power-on default high-impedance mode
 
-			bis.b	#(BIT0|BIT1), &P1DIR	;defino as LEDs como saída
-			bis.b	#(BIT0|BIT1), &P1OUT 	;fazer leds começarem acesas
+			bic.b	#(BIT5|BIT6), &P5DIR	; P5.5 e P5.6 definidos p/ entrada
+			bis.b	#(BIT5|BIT6), &P5REN 	; coloco as resistências
+			bis.b	#(BIT5|BIT6), &P5OUT 	; pullup
+
+			bis.b	#(BIT0|BIT1), &P1DIR	; defino as LEDs como saída
+			bic.b	#(BIT0|BIT1), &P1OUT 	; fazer leds começarem acesas
 
 loop:
-			bit.b	#BIT5, &P5IN
+			bit.b	#BIT5, &P5IN	;verifico se o botão está pressionado, caso esteja, o carry vai estar em 0
+			jc		loop			;se o botão não estiver pressionado, continuo esperando ele ser pressionado
+			call 	#reboteManager	;caso o botão tenha sido pressionado, chamo a função que esperará 2ms para cuidar do rebote
+			bit.b	#BIT5, &P5IN	;após os 2ms, onde o rebote já deve ter se estabilizado, verifico se o botão ainda está sendo pressionado, caso sim, sigo o código e caio em "on", onde o botão será ligado
 			jc		off				;se o botão estiver pressionado, bit5 é 0, logo, no teste, o carry quando não identifica bit em 1, é 0 também
 									;se o carry estiver em 1, significa que o bit testado é 1
 on:
 			bis.b	#BIT0, &P1OUT
 			jmp		loop
+			nop
 off:
 			bic.b	#BIT0, &P1OUT
-			jmp		loop
+			jmp 	loop
 			nop
 
-			;bis.b	#(BIT0|BIT1), &P1OUT ;fazer leds começarem acesas
+reboteManager:
+			mov.w	#10000, R6 	;faço R6 decrementar 10000 -> 16Mhz é o clock da MSP430
+								;jmp = 2 instr - dec = 1 -> 30000 instruções ---> 30000/16000000 -> 0.001875s
+loopRM:
+			dec		R6
+			jnz		loopRM
+			ret
 
-
-
-
-                                            
 
 ;-------------------------------------------------------------------------------
 ; Stack Pointer definition
