28804d8e1af31799d746d8240ff9326355b306ec
Estevam Albuquerque <estevamgalvao@gmail.com>
2019-06-29 07:45:27 -0300

    commentation

---
+++ FinalProject/finalProject.c
@@ -0,0 +1,86 @@
+#include <msp430.h>
+#include "lib/uart.h"
+
+uint8_t password[] = {0x41, 0x42, 0x43, 0x44};  //Vetor que administrará a senha para ascender o LED
+uint8_t counter = 0;                            //Contador para saber quantos caracteres o usuário acertou
+                                                //Dessa forma não preciso de outro vetor para guardar as tentativas do usuário
+
+/**
+ * main.c
+ */
+
+int main(void)
+{
+	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
+    PM5CTL0 &= ~LOCKLPM5;       // Disable the GPIO power-on default high-impedance mode
+
+	//Configurar pinos dos leds
+	//Configurar UART -> UCA3 6.1 RX 6.0 TX
+
+    P1DIR |= BIT0 | BIT1;                           //Configuro as LED como saída
+	P1OUT &= ~(BIT0 | BIT1);                        //Inicio as LED apagadas
+
+	UART_config(38400, LSB, WITHOUT, 0, BITS8);     //Baudrate de 38400bps, LSB first, sem paridade, 1 bit de STOP e 8BITS por comunicação
+
+    __enable_interrupt();                           //Habilito o General Interrupt Enable (GIE) no meu Status Register (R2)
+
+    UCA3IE |= (UCRXIE | UCTXIE);                    //Tive de habilitar as interrupções do RXBUF e TXBUF também na main porque mesmo habilitando
+                                                    //na chamada da função UART_config, os bits não estavam sendo "setados"
+    while(1);
+}
+
+
+#pragma vector=EUSCI_A3_VECTOR
+__interrupt void UAC3ISR(void) {
+    switch(UCA3IV) {
+    case 0x00:
+    // Vector 0: No interrupts
+    break;
+    case 0x02:  // Vector 2: UCRXIFG
+
+
+        if (UCA3RXBUF != password[counter]) {   //Se a enésima tentativa (counter) do usuário está incorreta, se estiver, reinicio a contagem
+            counter = 0;                        //e dou toggle no LED vermelho indicando o erro
+            P1OUT ^= BIT0;
+        }
+        else {
+            counter++;                          //Se o byte enviado foi correto, incremento o counter indicando que +1 byte foi acertado
+        }
+        if (counter == 4) {                     //No momento em que o 4º byte correto for lido, counter será = 4 e então devo ascender o LED verde
+            P1OUT ^= BIT1;                      //indicando que a senha correta foi inserida. Após isso zero o counter para receber uma nova tentativa
+            counter = 0;
+        }
+
+        //  Tentando debugar através das LED e comparando com o registro no debug do CCS
+        /*
+                if (UCA3RXBUF == 0x01) {
+                    P1OUT ^= BIT0;
+                    P1OUT &= ~BIT1;
+
+                }
+                else if (UCA3RXBUF == 0x42) {
+                    P1OUT &= ~BIT0;
+                    P1OUT |= BIT1;
+
+                }
+                else if (UCA3RXBUF == 0x43) {
+                    P1OUT |= BIT0;
+                    P1OUT &= ~BIT1;
+
+                }
+                else {
+                    P1OUT |= BIT0;
+                    P1OUT |= BIT1;
+                }
+        */
+    break;
+    case 0x04: // Vector 4: UCTXIFG
+    break;
+    case 0x06: // Vector 6: UCSTTIFG
+    break;
+    case 0x08: // Vector 8: UCTXCPTIFG
+    break;
+    default: break;
+    }
+
+}
--- FinalProject/lib/uart.c
+++ FinalProject/lib/uart.c
@@ -3,62 +3,66 @@
 #include "uart.h"
 
 void UART_config (uint32_t baudrate, uint8_t bitOrder, uint8_t parity, uint8_t nStops, uint8_t cLength) {
+	//UCA3CTLW0 PÁG 784 userGuide
 	
-	UCA3CTLW0 = UCSWRST;	
+    UCA3CTLW0 = UCSWRST;	            //Zero completamente o registrador e levanto apenas o bit UCSWRST
+	                                    //para garantir que o registrador irá começar de um estado que eu
+	                                    //conheço e habilito o modo de configuração
 
-	switch(parity) {
-	case ODD:
+	switch(parity) {                    //Crio um switch para as configurações de paridade do registrador (PAR, IMPAR, SEM)
+	case ODD:                           //PARIDADE PAR
 		UCA3CTLW0 |= UCPEN;
 		UCA3CTLW0 &= ~UCPAR;
 		break;			
-	case EVEN:
+	case EVEN:                          //PARIDADE ÍMPAR
 		UCA3CTLW0 |= UCPEN| UCPAR;
 		break;
-	case WITHOUT:
+	case WITHOUT:                       //SEM PARIDADE
 		UCA3CTLW0 &= ~UCPEN;
 		break;
 	default: break; 
 	}
 
-	if (bitOrder) {
-	    UCA3CTLW0 |= UCMSB;
+	if (bitOrder) {                     //Verifico qual a ordem de envio de bits que o usuário deseja, MSB first ou LSB first
+	    UCA3CTLW0 |= UCMSB;             //MSB first
 	}
 	else {
-	    UCA3CTLW0 &= ~UCMSB;
+	    UCA3CTLW0 &= ~UCMSB;            //LSB first
 	}
 
-	if (cLength) {
-		UCA3CTLW0 |= UC7BIT; //7 BITS
-	}
-	else {
-		UCA3CTLW0 &= ~UC7BIT; //8 BITS
-	}
-
-	if (nStops) {
-	    UCA3CTLW0 |= UCSPB;
+	if (cLength) {                      //Verifico o tamanho dos dados que o usuário deseja enviar
+		UCA3CTLW0 |= UC7BIT;            //7 BITS
 	}
 	else {
-	    UCA3CTLW0 &= ~UCSPB;
+		UCA3CTLW0 &= ~UC7BIT;           //8 BITS
+	}
+
+	if (nStops) {                       //Verifico o número de STOP que o usuário deseja configurar na comunicação (1 ou 2)
+	    UCA3CTLW0 |= UCSPB;             //2 STOP
+	}
+	else {
+	    UCA3CTLW0 &= ~UCSPB;            //1 STOP
 	}
 
 
-	UCA3CTLW0 |= UCMODE_0;
-	UCA3CTLW0 &= ~UCSYNC;
+	UCA3CTLW0 |= UCMODE_0;              //Seleciono o UART mode do periférico
+	UCA3CTLW0 &= ~UCSYNC;               //Seleciono o modo asíncrono do periférico, afinal estou trabalhando com uma comunicação UART
 
-	//Criar função que estima o melhor baudrate
+	UCA3CTLW0 |= UCSSEL_3;              //Sempre seleciono o SMCLK pois tem a melhor combinação com os baudrates suportados pelo dispositivo HC-05
 
-	UCA3CTLW0 |= UCSSEL_3;
 	uint32_t brDiv = (1000000  << 3)/baudrate; //descolo em 3bits (* 2^3) o 10^6 e faço a divisão pelo baudrate
 	                                           //dessa forma, é como se eu fizesse o número 104,16 em binário ficar 1041,6
 	UCA3BRW = brDiv >> 3;                      //então eu salvo a parte inteira dessa divisão, dividindo por 2^3, em BRW
 	UCA3MCTLW = (brDiv & 0x07) << 8;           //e finalmente, salvo os 3 bits menos significativos (nesse exemplo seriam o 1 = 001) em MCTLW
+	                                           //Desloco 8 bits pois UCBRS, na MSP430FR5994, fica entre os bits 15-8 // PÁG 786 userGuide
 
-	UCA3IE |= (UCRXIE | UCTXIE);
+	UCA3IE |= (UCRXIE | UCTXIE);               //Habilito as interrupções dos buffers de transmissão e recepção
 
-	//Portabilidade do pino PAG 107 datasheet
+	//Portabilidade dos pinos 6.1 e 6.0 // PAG 107 datasheet
+	//PINO 6.0 UCA3TXD
+	//PINO 6.1 UCA3RXD
 	P6SEL0 |= (BIT0|BIT1); 
 	P6SEL1 &= ~(BIT0|BIT1);
 
-
-	UCA3CTLW0 &= ~UCSWRST;
+	UCA3CTLW0 &= ~UCSWRST;                     //Desabilito o modo de configuração do registrador
 }
